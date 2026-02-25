b4426b8f9ab9872d238f223c985babd7165e1aa4
Soyprometeo <jgfkwow@gmail.com>
2026-02-25 20:03:07 -0300

    Add form submission handler with success message

---
+++ empresa/js/script.js
@@ -0,0 +1,5 @@
+function enviarFormulario(event){
+    event.preventDefault();
+    document.getElementById("respuesta").innerText =
+        "Mensaje enviado correctamente. Nos contactaremos pronto.";
+}
